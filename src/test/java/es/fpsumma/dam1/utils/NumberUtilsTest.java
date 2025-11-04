90758925cb102eab0055b5546361f44018bb008f
nicolasgarcia06 <805@alumnos.fpsumma.es>
2025-11-04 12:20:15 +0100

    Update NumberUtilsTest.java

--- src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
+++ src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
@@ -1,10 +1,21 @@
 package es.fpsumma.dam1.utils;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class NumberUtilsTest {
+@Test
+    public void sumaOK() {
+        int resultado = NumberUtils.suma(2, 3);
+        assertEquals(5,resultado);
+    }
 
-   
+   @Test
+   public void IsEven(){
+    Boolean resultado=NumberUtils.isEven(n % 2 == 0);
+    assertEquals(2,resultado);
+   }
+
 }
